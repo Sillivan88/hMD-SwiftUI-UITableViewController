4933406d023bc284a35f55b16fb01b7edd9c3050
Thomas Sillmann <thomas@thomassillmann.de>
2019-11-07 14:26:42 +0100

    Initial Commit

---
+++ hMD SwiftUI UITableViewController/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hMD SwiftUI UITableViewController
+//
+//  Created by Thomas Sillmann on 07.11.19.
+//  Copyright © 2019 Thomas Sillmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
